6fe7900e5459d7e0859d3e1a0de486ab63afd3cc
Marmaduke <arm77@254-11.cs.uakron.edu>
2015-04-10 18:51:13 -0400

    Add microwave description

--- microwave/gen.cpp
+++ microwave/gen.cpp
@@ -4,17 +4,18 @@ int main()
 {
    using namespace std;
 
-   cout << "14" << endl;
+   cout << "15" << endl;
 
-   cout << "0" << endl;
+   cout << "10" << endl;
    cout << "100" << endl;
+   cout << "0" << endl; 
    cout << "59" << endl;
-   cout << "9959" << endl;
+   cout << "9999" << endl;
 
    for (int i = 0; i < 10; ++i)
    {
       int hours = rand() % 99;
-      int minutes = rand() % 59;
+      int minutes = rand() % 99;
       cout << (100*hours + minutes) << endl;
    }
 }
