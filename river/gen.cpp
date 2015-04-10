acc8727bb4a343f8437cb4d19cf3584cd4b5f94e
Andrew Marmaduke <marmaduke.andrew@gmail.com>
2015-04-10 19:14:46 -0400

    Add river description

--- river/gen.cpp
+++ river/gen.cpp
@@ -5,10 +5,12 @@ int main()
    using namespace std;
 
    cout << "30" << endl;
+   cout << "2" << endl;
+   cout << "5" << endl;
    cout << "1" << endl;
    cout << "50" << endl;
 
-   for (int i = 0; i < 30 - 2; ++i)
+   for (int i = 0; i < 30 - 4; ++i)
    {
       int x = (rand() % 50) + 1;
       cout << x << endl;
