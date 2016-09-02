42fe8ba9d3bfc50f8dfff60eca391327a31a3bf0
Andrew Marmaduke <marmaduke.andrew@gmail.com>
2016-09-02 12:42:59 -0400

    Fall 2016 contest init

---
+++ Fall-2016/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+Cases=$2
+
+g++ $1/solution.cpp -DSINGLE_FILE -o $1/solution
+
+for ((i=1; i<=Cases; i++)); do
+   infile="$1/data/test$i.in"
+   outfile="$1/data/test$i.ans"
+   ($1/./solution < $infile) > $1/temp
+   echo "Trying $i..."
+   diff $1/temp $outfile
+done
+
+rm $1/temp
+rm $1/solution
