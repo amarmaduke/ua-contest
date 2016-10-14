bc5d9682f93811277dd2791021698271387c8acd
AaronBattershell <soccerman0010@gmail.com>
2016-10-13 22:32:37 -0400

    Adding beaver solution and tex file

---
+++ Fall-2016/beaver/solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+        string myDam;
+        string goalDam;
+        string actions;
+
+        cin >> myDam >> goalDam >> actions;
+
+        myDam = "0" + myDam + "0";
+        goalDam = "0" + goalDam + "0";
+
+	for (int i = 0, myPos = 1; i < actions.size() - 1 && myDam != goalDam; ++i) {
+		
+		if (actions[i] == 'R') {
+                        ++myPos;
+                } else if (actions[i] == 'L') {
+                        --myPos;
+                } else if (actions[i] == 'T') {
+                        myDam[myPos] = (myDam[myPos] == '0' ? '1' : '0');
+                } else if  (actions[i] == 'X') {
+                        myDam[myPos] = (myDam[myPos - 1] != myDam[myPos + 1] ? '1' : '0');
+                }
+	}
+
+        cout << (myDam == goalDam ? "YES" : "NO") << endl;
+
+        return 0;
+}
