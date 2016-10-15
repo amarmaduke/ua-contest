77b21ce03dc92b9a4da86ead54165d2d197af62d
Battershell <amb288@254-12.cs.uakron.edu>
2016-10-14 21:26:58 -0400

    Adding file for generating tests. I didn't test it. You should probably change the number of tests generated too.

---
+++ Fall-2016/beaver/data/generate.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <fstream>
+using namespace std;
+
+// Make bias towards moving right
+int TOTAL_ACTIONS = 5;
+char actions[] = { 'L', 'R', 'R', 'T', 'X' };
+
+string makeDam(int size) {
+	string str;
+
+	for (int i = 0; i < size; ++i) {
+		str += (rand() % 2 ? "0" : "1");
+	}
+
+	return str;
+}
+
+int main() {
+	int NUM_TESTS = 10000;
+	
+	srand(time(NULL));
+
+	// Generate tests by getting a dam config, then applying transforms while recording the actions
+
+	for (int test = 4; test <= NUM_TESTS; ++test) {
+		ofstream in("test" + to_string(test) + ".in");
+		ofstream ans("test" + to_string(test) + ".ans");
+		
+		string startDam = "0" + makeDam(rand() % 100 + 1) + "0";
+		string goalDam = startDam;
+		string actionList = "";
+
+		int numActions = rand() % 900 + 99;
+
+		int position = 1;
+		for (int actionNum = 0; actionNum < numActions; ++actionNum) {
+			char action = actions[rand() % TOTAL_ACTIONS];
+
+			// if illegal action panic
+			if ((action == 'L' && position == 1) || (action == 'R' && position == startDam.size() - 2)) {
+				--actionNum;
+				continue;
+			} 
+
+			actionList += action;
+
+			if (action == 'L') {
+				--position;				
+			} else if (action == 'R') {
+				++position;
+			} else if (action == 'T') {
+				goalDam[position] = (goalDam[position] == '0' ? '1' : '0');
+			} else if (action == 'X') {
+				goalDam[position] = (goalDam[position-1] == goalDam[position+1] ? '0' : '1');
+			} else {
+				cout << "wtf is going on" << endl;
+			}
+		}
+
+		in << startDam << endl << goalDam << endl << actionList << endl;
+		ans << "YES";
+	}
+
+	return 0;
+}
+
