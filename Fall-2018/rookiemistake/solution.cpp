bc8f49f37d06364b9b7e921bc5e7603d12726162
Cody Skala <cspluggs@gmail.com>
2018-11-16 21:30:09 -0500

    Change piece letters for better clarity

--- Fall-2018/rookiemistake/solution.cpp
+++ Fall-2018/rookiemistake/solution.cpp
@@ -29,52 +29,52 @@ int main()
     }
 
     for(int x = rook1x - 1; x > -1; --x){
-        if (board[x][rook1y] == 'k' || board[x][rook1y] == 'R')
+        if (board[x][rook1y] == 'K' || board[x][rook1y] == 'R')
             break;
-        if (board[x][rook1y] == 'K')
+        if (board[x][rook1y] == '$')
             cout << rook1x << " " << rook1y << " L " << rook1x - x << endl;
     }
     for(int x = rook1x + 1; x < 8; ++x){
-        if (board[x][rook1y] == 'k' || board[x][rook1y] == 'R')
+        if (board[x][rook1y] == 'K' || board[x][rook1y] == 'R')
             break;
-        if (board[x][rook1y] == 'K')
+        if (board[x][rook1y] == '$')
             cout << rook1x << " " << rook1y << " R " << x - rook1x << endl;
     }
     for(int y = rook1y - 1; y > -1; --y){
-        if (board[rook1x][y] == 'k' || board[rook1x][y] == 'R')
+        if (board[rook1x][y] == 'K' || board[rook1x][y] == 'R')
             break;
-        if (board[rook1x][y] == 'K')
+        if (board[rook1x][y] == '$')
             cout << rook1x << " " << rook1y << " U " << rook1y - y << endl;
     }
     for(int y = rook1y + 1; y < 8; ++y){
-        if (board[rook1x][y] == 'k' || board[rook1x][y] == 'R')
+        if (board[rook1x][y] == 'K' || board[rook1x][y] == 'R')
             break;
-        if (board[rook1x][y] == 'K')
+        if (board[rook1x][y] == '$')
             cout << rook1x << " " << rook1y << " D " << y - rook1y << endl;
     }
 
     for(int x = rook2x - 1; x > -1; --x){
-        if (board[x][rook2y] == 'k' || board[x][rook2y] == 'R')
+        if (board[x][rook2y] == 'K' || board[x][rook2y] == 'R')
             break;
-        if (board[x][rook2y] == 'K')
+        if (board[x][rook2y] == '$')
             cout << rook2x << " " << rook2y << " L " << rook2x - x << endl;
     }
     for(int x = rook2x + 1; x < 8; ++x){
-        if (board[x][rook2y] == 'k' || board[x][rook2y] == 'R')
+        if (board[x][rook2y] == 'K' || board[x][rook2y] == 'R')
             break;
-        if (board[x][rook2y] == 'K')
+        if (board[x][rook2y] == '$')
             cout << rook2x << " " << rook2y << " R " << x - rook2x << endl;
     }
     for(int y = rook2y - 1; y > -1; --y){
-        if (board[rook2x][y] == 'k' || board[rook2x][y] == 'R')
+        if (board[rook2x][y] == 'K' || board[rook2x][y] == 'R')
             break;
-        if (board[rook2x][y] == 'K')
+        if (board[rook2x][y] == '$')
             cout << rook2x << " " << rook2y << " U " << rook2y - y << endl;
     }
     for(int y = rook2y + 1; y < 8; ++y){
-        if (board[rook2x][y] == 'k' || board[rook2x][y] == 'R')
+        if (board[rook2x][y] == 'K' || board[rook2x][y] == 'R')
             break;
-        if (board[rook2x][y] == 'K')
+        if (board[rook2x][y] == '$')
             cout << rook2x << " " << rook2y << " D " << y - rook2y << endl;
     }
     return 0;
